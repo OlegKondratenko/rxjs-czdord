46a16436dd830be31d3498edff09437d5564aae7
OlegKondratenko <cybmad2@gmail.com>
2021-07-27 23:26:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-czdord
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-czdord)
---
+++ index.ts
@@ -0,0 +1,5 @@
+// Build a Mortgage Claculator using Rxjs and calculateMortgage method
+import { fromEvent } from 'rxjs';
+
+const loanAmountInput = document.getElementById('loanAmount');
+const loanInterestInput = document.getElementById('loanInterest');
